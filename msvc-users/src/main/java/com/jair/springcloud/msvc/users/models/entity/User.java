27917e975fd7afdb0b152961d51292c10260c87e
Jair0305 <chamaco_03@hotmail.es>
2024-01-06 01:54:35 -0600

    project intialized and added users microservice

---
+++ msvc-users/src/main/java/com/jair/springcloud/msvc/users/models/entity/User.java
@@ -0,0 +1,23 @@
+package com.jair.springcloud.msvc.users.models.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, length = 100)
+    private String email;
+
+    private String password;
+
+}
